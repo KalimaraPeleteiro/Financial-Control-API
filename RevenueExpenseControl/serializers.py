c91a7e355bfdc619bb16449b616d10e7bed38979
Kalimara_Peleteiro <kaliamrapeleteiro@gmail.com>
2022-08-03 09:00:06 -0300

    Adicionando Regras de Negocio

--- RevenueExpenseControl/serializers.py
+++ RevenueExpenseControl/serializers.py
@@ -5,10 +5,23 @@ from RevenueExpenseControl.models import *
 class ReceitasSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receita
-        fields = ['descricao', 'valor', 'data']
+        fields = '__all__'
+    
+    def validate(self, data):
+        query = Receita.objects.filter(descricao = data['descricao'])
+        for object in query:
+            if object.data.month == data['data'].month:
+                raise serializers.ValidationError({'descricao': "Uma receita com essa descrição já foi registrada esse mês."})
+        return data
 
 class DespesasSerializer(serializers.ModelSerializer):
     class Meta:
         model = Despesa
-        fields = ['descricao', 'valor', 'data']
-
+        fields = '__all__'
+    
+    def validate(self, data):
+        query = Despesa.objects.filter(descricao = data['descricao'])
+        for object in query:
+            if object.data.month == data['data'].month:
+                raise serializers.ValidationError({'descricao': "Uma despesa com essa descrição já foi registrada esse mês."})
+        return data
